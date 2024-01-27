61ec9db81edeb41d5448cd8412001d568256eb02
Amrit Rathie <amrit.rathie@gmail.com>
2024-01-27 16:10:00 -0600

    Add sync server

---
+++ syncserver/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Handle a simple custom message from clients
+  socket.on("syncData", (data) => {
+    console.log("Data received: ", data);
+    // Emit data to all connected clients
+    io.emit("syncData", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
