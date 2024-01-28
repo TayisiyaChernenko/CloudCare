691456736aefb5a4b6c28513e877a4cd54ff36dc
Charlie Mahana <git@charliemahana.com>
2024-01-28 01:31:24 -0600

    complete gesture tracking

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -5,58 +5,160 @@
 
   import Handsfree from "handsfree";
 
+  let xx = 0;
+  let yy = 0;
+
   onMount(() => {
     const handsfree = new Handsfree({
-      hands: {
-        enabled: true,
-        minDetectionConfidence: 0.8,
-        minTrackingConfidence: 0.7,
-      },
-    });
-    handsfree.enablePlugins("core");
-    handsfree.enablePlugins("browser");
-    handsfree.plugin.pinchScroll.enable();
-    handsfree.start();
-
-    handsfree.on("finger-pinched-0-0", () => {
-      // get position
-      let { x, y } = handsfree.data.hands.origPinch[0][0];
-      x = x * window.innerWidth;
-      y = y * window.innerHeight;
-
-      // check current position to see if user is dragging. if they are, don't fire click
-      if (handsfree.data.hands.curPinch[0][0].x * window.innerWidth !== x)
-        return;
-
-      // log where click would land
-      console.log(x, y);
-      console.log(document.elementFromPoint(x, y));
-
-      // click
-      document.elementFromPoint(x, y).click();
+        hands: {
+            enabled: true,
+            maxNumHands: 1,
+            minDetectionConfidence: 0.5,
+            minTrackingConfidence: 0.5,
+        },
+        plugin: {
+            palmPointers: {
+                enabled: true,
+                offset: {
+                    x: 0,
+                    y: 0,
+                },
+                speed: {
+                    x: 1.5,
+                    y: 1.5,
+                }
+            }
+        }
     });
 
-    handsfree.on("finger-pinched-1-0", () => {
-      // get position
-      let { x, y } = handsfree.data.hands.origPinch[1][0];
-      x = x * window.innerWidth;
-      y = y * window.innerHeight;
+    handsfree.start()
 
-      // check current position to see if user is dragging. if they are, don't fire click
-      if (handsfree.data.hands.curPinch[1][0].x * window.innerWidth !== x)
-        return;
+      let lastHeld = [{x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}]
 
-      // log where click would land
-      console.log(x, y);
-      console.log(document.elementFromPoint(x, y));
+      document.addEventListener("handsfree-data", event => {
+          const data = event.detail;
+          if (!data.hands) return;
+          if (!data.hands.pointer) return;
+          if (!data.hands.pointer[0]) return;
+          if (!data.hands.pointer[0].isVisible) return;
+          if (!data.hands.pinchState) return;
 
-      // click
-      document.elementFromPoint(x, y).click();
-    });
-  });
+          let hand = data.hands
+          let pointer = hand.pointer[0];
+
+          console.log(hand);
+
+          const eventMap = {
+              start: 'mousedown',
+              held: 'mousemove',
+              released: 'mouseup',
+          }
+
+          const pinch = hand.pinchState?.[0]?.[0];
+          if (!pinch) {
+              return;
+          }
+
+          const $el = document.elementFromPoint(pointer.x, pointer.y)
+          if ($el) {
+              $el.dispatchEvent(
+                  new MouseEvent(eventMap[pinch], {
+                      view: window,
+                      button: 0,
+                      bubbles: true,
+                      cancelable: true,
+                      clientX: pointer.x,
+                      clientY: pointer.y,
+                      movementX: pointer.x - lastHeld[0].x,
+                      movementY: pointer.y - lastHeld[0].y,
+                      }
+                  )
+              )
+
+              if (eventMap[pinch] === 'mousedown') {
+                  $el.dispatchEvent(
+                  new MouseEvent('click', {
+                      view: window,
+                      button: 0,
+                      bubbles: true,
+                      cancelable: true,
+                      clientX: pointer.x,
+                      clientY: pointer.y,
+                      movementX: pointer.x - lastHeld[0].x,
+                      movementY: pointer.y - lastHeld[0].y,
+                      }
+                  )
+              )
+              }
+          }
+          lastHeld[0] = pointer;
+      })
+
+      document.addEventListener("handsfree-data", event => {
+          const data = event.detail;
+          if (!data.hands) return;
+          if (!data.hands.pointer) return;
+          if (!data.hands.pointer[1]) return;
+          if (!data.hands.pointer[1].isVisible) return;
+          if (!data.hands.pinchState) return;
+
+          let hand = data.hands
+          let pointer = hand.pointer[1];
+
+          console.log(hand);
+
+          const eventMap = {
+              start: 'mousedown',
+              held: 'mousemove',
+              released: 'mouseup',
+          }
+
+          const pinch = hand.pinchState?.[1]?.[0];
+          if (!pinch) {
+              return;
+          }
+
+          const $el = document.elementFromPoint(pointer.x, pointer.y)
+          if ($el) {
+              $el.dispatchEvent(
+                  new MouseEvent(eventMap[pinch], {
+                      view: window,
+                      button: 0,
+                      bubbles: true,
+                      cancelable: true,
+                      clientX: pointer.x,
+                      clientY: pointer.y,
+                      movementX: pointer.x - lastHeld[1].x,
+                      movementY: pointer.y - lastHeld[1].y,
+                      }
+                  )
+              )
+
+              if (eventMap[pinch] === 'mousedown') {
+                  $el.dispatchEvent(
+                  new MouseEvent('click', {
+                      view: window,
+                      button: 0,
+                      bubbles: true,
+                      cancelable: true,
+                      clientX: pointer.x,
+                      clientY: pointer.y,
+                      movementX: pointer.x - lastHeld[1].x,
+                      movementY: pointer.y - lastHeld[1].y,
+                      }
+                  )
+              )
+              }
+          }
+          lastHeld[1] = pointer;
+      })
+  })
 </script>
 
-<slot />
+<div>
+    <slot />
+</div>
+
 
 <style>
   :global(.handsfree-pointer) {
