289fb52b5aefa919c63377c7a8258848b7f41065
Tayisiya Chernenko <tayisiyachernenko@Tayisiyas-MacBook-Air.local>
2024-01-28 10:18:50 -0600

    Populated games

--- website/src/routes/passenger/games/+page.svelte
+++ website/src/routes/passenger/games/+page.svelte
@@ -3,103 +3,48 @@
     import NavBar from "../../../components/NavBar.svelte";
 
     let cards = [
-        {
-            title: 'Ted',
-            description: 'This is a test',
+       {
+            title: 'Candy Crush',
+            description: 'A Colorful Challenge Game',
             status: false,
-            src: "https://m.media-amazon.com/images/I/41DsVUWsCLL._AC_UF894,1000_QL80_.jpg"
+            src: "https://k1.midasplayer.com/images/share/banners/candycrush.png?_v=kmoqjd"
         },
         {
-            title: 'Over the Hedge',
-            description: '',
+            title: 'Chess',
+            description: 'All Levels Strategy Game',
             status: false,
-            src: "https://m.media-amazon.com/images/I/516V1BUK6xL._AC_UF894,1000_QL80_.jpg"
+            src: "https://www.chess-space.com/wp-content/uploads/2012/11/CHESS.png"
         },
         {
-            title: '',
-            description: '',
+            title: 'Checkers',
+            description: 'Fun for kids',
             status: false,
-            src: "https://i5.walmartimages.com/seo/Top-Gun-Maverick-Movie-Poster-Quality-Glossy-Print-Photo-Wall-Art-Tom-Cruise-Jennifer-Connelly-Size-24x36_bbb92289-b359-423e-a8fe-6142b188f677.4da7e4af2871002298f4804afb44b955.jpeg" },
+            src: "https://www.casualarena.com/bundles/app/st/games/che/fb/casualarena-fb-en.jpg?4188916" },
         {
-            title: '',
-            description: '',
+            title: 'Sudoko',
+            description: 'A Numbers Brain-Puzzle Game',
             status: false,
-            src: "https://m.media-amazon.com/images/I/71Tpd21hEbL._AC_UF894,1000_QL80_.jpg" },
+            src: "https://www.my-sudoku.com/images/snap_banner.jpg" },
         {
-            title: '',
-            description: '',
+            title: 'Crossy Road',
+            description: 'Fun and Simple',
             status: false,
-            src: "https://m.media-amazon.com/images/I/91zqGNzwk5L._AC_UF894,1000_QL80_.jpg" },
+            src: "https://mobiledevmemo.com/wp-content/uploads/2015/04/crossy_road.jpg" },
         {
-            title: '',
-            description: '',
+            title: 'Cross Word',
+            description: 'A Word-Based Brain Teaser',
             status: false,
-            src: "https://m.media-amazon.com/images/I/71BgdzmFDAL._AC_UF894,1000_QL80_.jpg" },
+            src: "https://play-lh.googleusercontent.com/DjuowCxLuHPSUrqfkXEjlYB92VUnvHWctWOS8WyC437G-LT-aNSepN30D7NtecfkQ2c" },
         {
-            title: '',
-            description: '',
+            title: 'Poker',
+            description: 'A Game of Wits',
             status: false,
-            src: "https://i.pinimg.com/originals/66/bd/9b/66bd9b1f8354a9aa6900dee35bc2a911.jpg" },
+            src: "https://smartcasinoguide.com/app/uploads/2018/01/Casino-hold-em-poker-casino-holdem-bonus-bet-online-gameplay-table.jpg" },
         {
-            title: '',
-            description: '',
+            title: 'Pool',
+            description: 'A Game of Geometry and Precision',
             status: false,
-            src: "https://i.etsystatic.com/41716657/r/il/9c990a/4742863548/il_570xN.4742863548_patj.jpg" },
-        {
-            title: '',
-            description: '',
-            status: false,
-            src: "https://m.media-amazon.com/images/I/51b6mLCvBuL._AC_UF894,1000_QL80_.jpg" },
-        {
-            title: '',
-            description: '',
-            status: false,
-            src: "https://m.media-amazon.com/images/M/MV5BMDJhMGRjN2QtNDUxYy00NGM3LThjNGQtMmZiZTRhNjM4YzUxL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg" },
-        {
-            title: '',
-            description: '',
-            status: false,
-            src: "https://m.media-amazon.com/images/I/91K98RNBBWL.jpg" },
-        {
-            title: '',
-            description: '',
-            status: false,
-            src: "https://m.media-amazon.com/images/I/61f1bYNLpKL._AC_UF1000,1000_QL80_.jpg" },
-        {
-            title: '',
-            description: '',
-            status: false,
-            src: "https://m.media-amazon.com/images/I/71U8fIXlGHL._AC_UF894,1000_QL80_.jpg" },
-        {
-            title: '',
-            description: '',
-            status: false,
-            src: "https://m.media-amazon.com/images/I/A1M0qlxNo6L.jpg" },
-        {
-            title: '',
-            description: '',
-            status: false,
-            src: "https://m.media-amazon.com/images/I/81ZCRFRoXYL._AC_UF894,1000_QL80_.jpg" },
-        {
-            title: '',
-            description: '',
-            status: false,
-            src: "https://m.media-amazon.com/images/I/81oON83v4WL._AC_UF894,1000_QL80_.jpg" },
-        {
-            title: '',
-            description: '',
-            status: false,
-            src: "https://pbncanvas.com/wp-content/uploads/2022/02/Knives-Out-movie-poster-paint-by-numbers.jpg" },
-        {
-            title: '',
-            description: '',
-            status: false,
-            src: "https://m.media-amazon.com/images/I/61c11xwXFjL._AC_UF894,1000_QL80_.jpg" },
-        {
-            title: '',
-            description: '',
-            status: false,
-            src: "https://i.ebayimg.com/images/g/qHwAAOSw1Y5dcuMC/s-l1600.jpg" },
+            src: "https://games.lol/wp-content/uploads/2019/07/pool-billiards-pro-pc-full-version.jpg" },
     ]
 </script>
 
