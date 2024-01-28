eaae485f53325c0f7fd28ab22d0ed2d923b06bf8
Charlie Mahana <git@charliemahana.com>
2024-01-28 08:53:25 -0600

    update navbar links

--- website/src/components/NavBar.svelte
+++ website/src/components/NavBar.svelte
@@ -13,11 +13,11 @@
         <img src={logo} class="flex absolute justify-center items-center h-20 w-20 p-2 mx-5">
     </a>
     <div class="flex w-screen justify-center items-center gap-5">
-        <a href="" class="flex justify-center items-center h-14 w-36 text-xl bg-aa-dark-blue rounded-2xl text-aa-grey font-bold">Movies</a>
-        <a href="" class="flex justify-center items-center h-14 w-36 text-xl bg-aa-dark-blue rounded-2xl text-aa-grey font-bold">Television</a>
-        <a href="" class="flex justify-center items-center h-14 w-36 text-xl bg-aa-dark-blue rounded-2xl text-aa-grey font-bold">Music</a>
-        <a href="" class="flex justify-center items-center h-14 w-36 text-xl bg-aa-dark-blue rounded-2xl text-aa-grey font-bold">Games</a>
-        <a href="" class="flex justify-center items-center h-14 w-36 text-xl bg-aa-dark-blue rounded-2xl text-aa-grey font-bold">My Flight</a>
+        <a href="/passenger/movies" class="flex justify-center items-center h-14 w-36 text-xl bg-aa-dark-blue rounded-2xl text-aa-grey font-bold">Movies</a>
+        <a href="/passenger/television" class="flex justify-center items-center h-14 w-36 text-xl bg-aa-dark-blue rounded-2xl text-aa-grey font-bold">Television</a>
+        <a href="/passenger/music" class="flex justify-center items-center h-14 w-36 text-xl bg-aa-dark-blue rounded-2xl text-aa-grey font-bold">Music</a>
+        <a href="/passenger/games" class="flex justify-center items-center h-14 w-36 text-xl bg-aa-dark-blue rounded-2xl text-aa-grey font-bold">Games</a>
+        <a href="/passenger/flight-info" class="flex justify-center items-center h-14 w-36 text-xl bg-aa-dark-blue rounded-2xl text-aa-grey font-bold">My Flight</a>
     </div>
     <div class="flex absolute right-0 justify-center items-center h-20 w-40 p-2 mx-5 text-aa-white font-bold text-xl">
         {String(currentTime.getHours()).padStart(2, '0')}:{String(currentTime.getMinutes()).padStart(2, '0')}:{String(currentTime.getSeconds()).padStart(2, '0')}
---
+++ website/src/routes/passenger/television/+page.svelte
@@ -0,0 +1,108 @@
+<script>
+    import CardCarousel from "../../../components/CardCarousel.svelte";
+    import NavBar from "../../../components/NavBar.svelte";
+
+    let cards = [
+        {
+            title: 'Ted',
+            description: 'This is a test',
+            status: false,
+            src: "https://m.media-amazon.com/images/I/41DsVUWsCLL._AC_UF894,1000_QL80_.jpg"
+        },
+        {
+            title: 'Over the Hedge',
+            description: '',
+            status: false,
+            src: "https://m.media-amazon.com/images/I/516V1BUK6xL._AC_UF894,1000_QL80_.jpg"
+        },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://i5.walmartimages.com/seo/Top-Gun-Maverick-Movie-Poster-Quality-Glossy-Print-Photo-Wall-Art-Tom-Cruise-Jennifer-Connelly-Size-24x36_bbb92289-b359-423e-a8fe-6142b188f677.4da7e4af2871002298f4804afb44b955.jpeg" },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://m.media-amazon.com/images/I/71Tpd21hEbL._AC_UF894,1000_QL80_.jpg" },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://m.media-amazon.com/images/I/91zqGNzwk5L._AC_UF894,1000_QL80_.jpg" },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://m.media-amazon.com/images/I/71BgdzmFDAL._AC_UF894,1000_QL80_.jpg" },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://i.pinimg.com/originals/66/bd/9b/66bd9b1f8354a9aa6900dee35bc2a911.jpg" },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://i.etsystatic.com/41716657/r/il/9c990a/4742863548/il_570xN.4742863548_patj.jpg" },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://m.media-amazon.com/images/I/51b6mLCvBuL._AC_UF894,1000_QL80_.jpg" },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://m.media-amazon.com/images/M/MV5BMDJhMGRjN2QtNDUxYy00NGM3LThjNGQtMmZiZTRhNjM4YzUxL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg" },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://m.media-amazon.com/images/I/91K98RNBBWL.jpg" },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://m.media-amazon.com/images/I/61f1bYNLpKL._AC_UF1000,1000_QL80_.jpg" },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://m.media-amazon.com/images/I/71U8fIXlGHL._AC_UF894,1000_QL80_.jpg" },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://m.media-amazon.com/images/I/A1M0qlxNo6L.jpg" },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://m.media-amazon.com/images/I/81ZCRFRoXYL._AC_UF894,1000_QL80_.jpg" },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://m.media-amazon.com/images/I/81oON83v4WL._AC_UF894,1000_QL80_.jpg" },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://pbncanvas.com/wp-content/uploads/2022/02/Knives-Out-movie-poster-paint-by-numbers.jpg" },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://m.media-amazon.com/images/I/61c11xwXFjL._AC_UF894,1000_QL80_.jpg" },
+        {
+            title: '',
+            description: '',
+            status: false,
+            src: "https://i.ebayimg.com/images/g/qHwAAOSw1Y5dcuMC/s-l1600.jpg" },
+    ]
+</script>
+
+<div class="card-carousel">
+    <CardCarousel cards={cards} />
+</div>
