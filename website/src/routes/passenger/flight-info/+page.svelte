c4bc6ac63708568727efac79fcfc1bf85af7af25
Adam Szumski <a.szumski98@gmail.com>
2024-01-28 03:23:34 -0600

    Add some flight info

--- website/src/routes/passenger/flight-info/+page.svelte
+++ website/src/routes/passenger/flight-info/+page.svelte
@@ -1,7 +1,12 @@
 <script>
+    import { onMount } from "svelte";
+    import Map from "../../../components/Map.svelte";
+	import NavBar from "../../../components/NavBar.svelte";
+
 	let queryCity = "Dallas";
 	let queryState = "TX";
 	let queryCountry = "US";
+	let weather;
 	// queryCity = Dallas
   	// queryState = Texas
   	// queryCountry = US
@@ -18,6 +23,7 @@
 					throw new Error(`API request failed with status ${response.status}`);
 				}
 				const currentWeather = await response.json();
+				weather = currentWeather;
 				console.log(currentWeather);
 			} catch (err) {
 				return { error: err.message};
@@ -26,8 +32,41 @@
 			return { error: err.message};
 		}
 	};
+	onMount(async () => {
+		await getCurrentWeather();
+	})
 </script>
 
-<div>
-	Hiiiii
+<div class="w-screen">
+	<div class="flex flex-row gap-8 p-8">
+		<div id="map" class="cols-span-3 bg-aa-grey w-2/3 rounded-xl">
+			<Map />
+		</div>
+		<div class="flex flex-col gap-8 w-1/3">
+			<div id="weather" class="bg-aa-grey text-aa-white rounded-xl h-64 p-6">
+				<p>Destination</p>
+				<h1 class="text-4xl"><b>{queryCity + " " + queryState}</b></h1>
+			</div>
+			<div id="connections" class="bg-aa-grey rounded-xl h-64 p-6 text-aa-white">
+				<div class="grid grid-cols-2">
+					<div class="text-left">
+						<p>Transfer Time</p>
+						<h1 class="text-5xl"><b>25 min</b></h1>
+					</div>
+					<div class="text-right">
+						<p>Arrival</p>
+						<p class="text-xl"><b>Terminal 5</b></p>
+						<p class="text-xl"><b>Gate 10</b></p>
+						<br>
+						<p>Connection</p>
+						<p class="text-xl"><b>Terminal 5</b></p>
+						<p class="text-xl"><b>Gate 18</b></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+	
+
 </div>
